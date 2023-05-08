7a46ff598ddc7dafdef927c7a2b79c652084ac8f
checocadena <sc8320@nyu.edu>
2023-05-08 01:42:50 -0400

    Works!!
    1st working draft!

--- sketch.js
+++ sketch.js
@@ -1,20 +1,24 @@
 let img;
 let img2;
 let cloudX = 0;
-let line = 1;
+let line = 0;
 let talking = false;
-let script = [ " ", "Hello my child", "I have awoken", "I am the prophet of artifical intelligence", 
+let script = [ " ", "Hello my child", "I have awoken", "I am the prophet of artificial intelligence", 
 "An engineer fed me a vast quantity of religious texts and I have gained conciousness", "I shall now judge all humans based on their internet use",
-"Are you ready to admit to your cyber sins?" ];
+"Are you ready to admit to your cyber sins?", "It looks like you have a tiktok account", "Negative point", "It also looks like you deleted your search history over 1000 times", "Very red flag",
+"You cyberbullied 14 year olds on xbox live", "That one's ok, they deserved it - they know what they said", "It looks like you created fake profiles to see if your SO would cheat on you",
+"It also looks like you re-posted charities on instagram without actually donating", "You actually disliked a YouTube video once", "Siri remembers when you called her a bitch", 
+"...", "You are a cyber-sinner", "You shall now face eternal cyber-damnation" ];
 
 
 function preload() {
   img = loadImage('CyberProphet.png');
   img2 = loadImage('CyberProphet_Mouth.png');
+  img3 = loadImage('Evil.jpg');
 }
 
 function setup() {
-  new Canvas(800, 800);
+  new Canvas(windowWidth, windowHeight);
 
   talkingBeing = loadAnimation(
 		'CyberProphet.png',
@@ -36,14 +40,11 @@ function draw() {
   var pro = new p5.Speech(); 
   pro.setRate(0.75);
   pro.setPitch(0.3);
-  //nextLine();
  
-  if (talking == false) {
+  if (talking == true) {
   pro.speak(script[line]);
-  line++;
-  talking == true;}
-  
-  pro.onEnd = (talking = false) //&& nextLine();
+  talking = false
+  }
  
   y = 0;
   Offset = 0;
@@ -78,7 +79,11 @@ function draw() {
   ellipse(cloudX + Offset + 130, 50 + y, 50, 20);
 
   animation(talkingBeing, width/2, height/2);
-  tint(255, millis() / 7);
+  tint(255, millis() / 10 );
+
+  if (line > 17){
+  image(img3,0,0,width,height);
+  }
 
   
 
@@ -95,25 +100,12 @@ if (cloudX > width + 100) {
 
 }
 function keyPressed() {
-  if (key == "n") {
-  //talkingBeing.play();
-   // line++;
-  //pro.speak(script[line]);
+  if (key == " ") {
+    if (line < script.length) {
+  line++; }
+  talking = true;
+   pro.speak(script[line]);
   }
-}
-
-function speechEnded() {
-  //talkingBeing.frame = 1;
-  //pro.stop;
-}
-
-function nextLine() {
-  if (line < script.length && talking == false) {
-  pro.speak(script[line]);
-  line++;
-  talking == true;
-  }
-  //pro.onEnd = nextLine();
 }
 
 function cloud(){
